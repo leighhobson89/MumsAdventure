e4ffc0073359590ff3a000032149c5bc8c6ad983
Leigh Hobson <leighhobson89@aol.com>
2022-09-20 00:20:44 +0200

    rockery tile added in

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -84,6 +84,9 @@ public class TileManager {
             setup(52, "kitCup7", true);
             setup(53, "kitCup8", true);
             setup(54, "mumsChairTile", false);
+            setup(55, "rockery4", true);
+            setup(56, "rockery5", true);
+            setup(57, "rockery6", true);
 
     }
 
